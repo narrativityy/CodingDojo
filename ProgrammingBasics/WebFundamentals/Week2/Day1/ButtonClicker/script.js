fae1af43c285bbd134072a1f1f063f07b25b9c81
narrativityy <jake_crowley@comcast.net>
2023-09-11 11:46:53 -0700

    Added Week 2 Day 1 Content

---
+++ ProgrammingBasics/WebFundamentals/Week2/Day1/ButtonClicker/script.js
@@ -0,0 +1,16 @@
+function login(elem) {
+    var inner = elem.innerText
+    if (inner == "Login")
+        elem.innerText = "Logout"
+    else {
+        elem.innerText = "Login"
+    }
+}
+
+function remove(elem) {
+    elem.remove()
+}
+
+function like() {
+    alert("Ninja was liked")
+}
