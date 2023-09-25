715dd459b98843d6dc698aa371d99fb1fb6a2561
narrativityy <jake_crowley@comcast.net>
2023-09-25 11:11:33 -0700

    Added For Loop Basic 1

--- Python/Week1/Day1/hello_world.py
+++ Python/Week1/Day1/hello_world.py
@@ -16,4 +16,3 @@ fave_food1 = "sushi"
 fave_food2 = "pizza"
 print("I love to eat {} and {}.".format(fave_food1, fave_food2)) # with .format()
 print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
-
