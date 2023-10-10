c2d73cbfb385c9f131f1e0bbf5e8beb5a828236d
narrativityy <jake_crowley@comcast.net>
2023-10-10 08:51:54 -0700

    Started Recipes

---
+++ Python/Week3/Day1/Recipe/flask_app/controllers/controller_recipes.py
@@ -0,0 +1,40 @@
+from flask_app.models.model_recipes import Recipe
+from flask_app import app
+from flask import render_template, request, redirect, session
+
+# CRUD
+
+# CREATE
+@app.post('/add_animal')
+def add_animal():
+    print (request.form)
+    Animal.create_one(request.form)
+    return redirect('/')
+
+# READ
+@app.route('/')
+def index():
+    animals = Animal.get_one(1)
+    return render_template('index.html', animals = animals)
+
+@app.route('/get/<int:id>')
+def get(id):
+    animal = Animal.get_one(id)
+    return animal.name
+
+# UPDATE
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+@app.post('/update/submit')
+def submit_update():
+    data = request.form
+    Animal.update(data)
+    return redirect('/')
+
+# DELETE
+@app.route('/delete/<int:id>')
+def delete(id):
+    Animal.delete(id)
+    return redirect('/')
---
+++ Python/Week3/Day1/Recipe/flask_app/models/model_recipes.py
@@ -0,0 +1,58 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Recipe:
+    def __init__(self, data):
+        self.id = data['id']
+        self.breed = data['breed']
+        self.age = int(data['age'])
+        self.fav_food = data['fav_food']
+        self.name = data['name']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    def info(self):
+        returnStr = f"Name = {self.name} || Age = {self.age} || Breed = {self.breed} || Favorite Food = {self.fav_food}"
+        return returnStr
+    
+
+    @classmethod
+    def create_one(cls, data:dict):
+        query = "INSERT INTO animals.animals (breed, age, fav_food, name) VALUES(%(breed)s, %(age)s, %(fav_food)s, %(name)s);"
+        animal_id = connectToMySQL('animals').query_db(query, data)
+        return animal_id
+
+
+    @classmethod
+    def get_all(cls) -> list:
+
+        query = "SELECT * FROM animals.animals;"
+
+        results = connectToMySQL('animals').query_db(query)
+
+        instance_list = []
+
+        for dict in results:
+            instance_list.append(cls(dict))
+
+        return instance_list
+    
+    @classmethod
+    def get_one(cls, id):
+
+        query = "SELECT * FROM animals.animals WHERE id = %(id)s;"
+        data = {'id':id}
+        results = connectToMySQL('animals').query_db(query, data)
+        return cls(results[0])
+
+    @classmethod
+    def update(cls, data):
+        query = """UPDATE animals SET breed=%(breed)s,age=%(age)s,fav_food=%(fav_food)s,
+        name=%(name)s WHERE id = %(id)s;"""
+        return connectToMySQL('animals').query_db(query, data)
+
+    @classmethod
+    def delete(cls, id):
+        query = "DELETE FROM animals WHERE id = %(id)s;"
+        data = {'id': id}
+        return connectToMySQL('animals').query_db(query, data)
