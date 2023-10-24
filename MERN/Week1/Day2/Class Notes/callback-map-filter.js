d717ede9d4b4bf0600bd32076c331953f6dc7889
narrativityy <jake_crowley@comcast.net>
2023-10-24 14:07:34 -0700

    added class notes

--- MERN/Week1/Day2/Class Notes/callback-map-filter.js
+++ MERN/Week1/Day2/Class Notes/callback-map-filter.js
@@ -8,5 +8,38 @@ setTimeout(myCoolCallback, 3000)
 
 console.log("end")
 
-const array1 = [1, 2, 4, 8]
-let array2 = array1.map((x) => console.log(x))
+const animals = ["leopard", "giraffe", "zebra", "elephant", "monkey", "lion"]
+const newAnimals = animals.map((elem) => {
+    return elem
+})
+
+console.log(newAnimals)
+
+const filteredAnimals = animals.filter((elem) => {
+    return elem.length < 7
+})
+
+console.log(filteredAnimals)
+
+const people = Object.freeze([
+    {name: "Bob", age: 30},
+    {name: "Mary", age: 31},
+    {name: "Sue", age: 28},
+    {name: "George", age: 38}
+])
+
+const newPeople = people.map((person) => {
+    return `${person.name} is cool`
+})
+
+console.log(newPeople)
+
+const filteredPeople = people.filter((person) => {
+    return person.age >= 30
+})
+
+// .filter() creates a shallow copy, which if changed will change the original
+filteredPeople[0].name = "unicorn"
+
+console.log(filteredPeople)
+console.log(people)
