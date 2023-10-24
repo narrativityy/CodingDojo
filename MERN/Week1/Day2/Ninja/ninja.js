9b0e1e1bcb6f1435466bf8d96d0146296f0605eb
narrativityy <jake_crowley@comcast.net>
2023-10-24 10:39:34 -0700

    added ninja assignment

---
+++ MERN/Week1/Day2/Ninja/ninja.js
@@ -0,0 +1,25 @@
+class Ninja {
+    constructor(name, health = 90, speed = 3, strength = 3) {
+        this.name = name
+        this.health = health
+        this.speed = speed
+        this.strength = strength
+    }
+
+    sayName() {
+        console.log(this.name)
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name} | Strength: ${this.strength} | Speed: ${this.speed} | Health: ${this.health}`)
+    }
+
+    drinkSake() {
+        this.health += 10
+    }
+}
+
+const ninja = new Ninja('jake')
+ninja.showStats()
+ninja.drinkSake()
+ninja.showStats()
