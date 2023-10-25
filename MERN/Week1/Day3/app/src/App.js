d6f73fc1611af7134bf9b7755afbd094a40900da
narrativityy <jake_crowley@comcast.net>
2023-10-25 10:29:49 -0700

    added First JSX assignment

---
+++ MERN/Week1/Day3/app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import HelloDojo from './components/HelloDojo';
+
+function App() {
+  return (
+    <HelloDojo />
+  );
+}
+
+export default App;
---
+++ MERN/Week1/Day3/app/src/components/HelloDojo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HelloDojo = () => {
+  return (
+    <div>
+        <h1>Hello Dojo!</h1>
+        <h3>Things I need to do:</h3>
+        <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marothon</li>
+            <li>Feed the dogs</li>
+        </ul>
+    </div>
+  )
+}
+
+export default HelloDojo
