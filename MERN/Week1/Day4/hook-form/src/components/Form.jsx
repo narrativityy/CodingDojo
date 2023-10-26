de7fba326882947fa90e3429ef84737f97247aac
narrativityy <jake_crowley@comcast.net>
2023-10-26 10:34:58 -0700

    added More Forms assignment

--- MERN/Week1/Day4/hook-form/src/components/Form.jsx
+++ MERN/Week1/Day4/hook-form/src/components/Form.jsx
@@ -12,6 +12,10 @@ const Form = (props) => {
     setFirstName(e.target.value)
   }
 
+  const validateFirstName = () => {
+    firstName.length < 2 && <p>First Name must be at least 2 characters</p>
+  }
+
   const createLastName = (e) => {
     setLastName(e.target.value)
   }
@@ -32,10 +36,15 @@ const Form = (props) => {
     <div>
       <form>
         <p>First Name: <input type="text" onChange={(e) => createFirstName(e)} /></p>
+        { firstName.length < 2 ? <p>First Name must be at least 2 characters</p> : '' }
         <p>Last Name: <input type="text" onChange={(e) => createLastName(e)} /></p>
+        { lastName.length < 2 ? <p>Last Name must be at least 2 characters</p> : '' }
         <p>Email: <input type="email" onChange={(e) => createEmail(e)} /></p>
+        { email.length < 5 ? <p>Email must be at least 5 characters</p> : '' }
         <p>Password: <input type="password" onChange={(e) => createPassword(e)} /></p>
         <p>Confirm Password: <input type="password" onChange={(e) => createConfirmPassword(e)} /></p>
+        { confirmPassword.length < 8 || password.length < 8 ? <p>Passwords must be at least 8 characters</p> : '' }
+        { password !== confirmPassword ? <p>Passwords must match</p> : '' }
       </form>
       <p>Your Form Data</p>
       <p>First Name: {firstName}</p>
