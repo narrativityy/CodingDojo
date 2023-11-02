ded0cf0e92f2953effd3d3eecd8bdb04e235dd48
narrativityy <jake_crowley@comcast.net>
2023-11-02 15:02:53 -0700

    added class notes

--- MERN/Week2/Day4/algo.js
+++ MERN/Week2/Day4/algo.js
@@ -69,7 +69,7 @@
 
 function twoSum(arr, target) {
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 1; j < arr.length; j++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] + arr[j] === target)
                 return [i, j]
         }
