d2ca30e2eb721daa032bf9d394324870e6945ff1
narrativityy <jake_crowley@comcast.net>
2023-11-30 13:50:27 -0800

    updated class notes

---
+++ C#/Week1/Day4/ClassNotes/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ClassNotes.Models;
+
+namespace ClassNotes.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("submit")]
+    public IActionResult SubmitPet(Pet newPet) {
+        if (!ModelState.IsValid)  {
+            Console.WriteLine("There was an error");
+            return View("Index");
+        }
+
+        System.Console.WriteLine($"Name: {newPet.Name}");
+        System.Console.WriteLine($"Type: {newPet.Type}");
+        System.Console.WriteLine($"Age: {newPet.Age}");
+        System.Console.WriteLine($"Favorite Food: {newPet.FavoriteFood}");
+
+        return View("DisplayPet", newPet);
+        // return Redirect("/");
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ C#/Week1/Day4/ClassNotes/Models/Pet.cs
@@ -0,0 +1,56 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassNotes.Models;
+
+public class Pet {
+    [Required]
+    [NoZNames]
+    public string? Name {get;set;}
+    [Required]
+    [NoHumansAsPets]
+    public string? Type {get;set;}
+    [Required(ErrorMessage = "Age is required.")]
+    [Range(0, 120)]
+    public int? Age {get;set;}
+    [Display(Name = "Your Pet's Favorite Food")]
+    public string? FavoriteFood {get;set;}
+}
+
+// Create a class that inherits from ValidationAttribute
+public class NoZNamesAttribute : ValidationAttribute
+{    
+    // Call upon the protected IsValid method
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)    
+    {   
+        if(value == null) {
+            // return new ValidationResult("this is a required field");
+            return ValidationResult.Success;
+        }
+        // We are expecting the value coming in to be a string
+        // so we need to do a bit of type casting to our object
+        // Strings work similarly to arrays under the hood 
+        // so we can grab the first letter using its index   
+        // If we discover that the first letter of our string is z...  
+        if (((string)value).ToLower()[0] == 'z')
+        {        
+            // we return an error message in ValidationResult we want to render    
+            return new ValidationResult("No names that start with Z allowed!");   
+        } else {   
+            // Otherwise, we were successful and can report our success  
+            return ValidationResult.Success;  
+        }  
+    }
+}
+
+public class NoHumansAsPetsAttribute : ValidationAttribute {
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)    
+    {   
+        if (value == null) {
+            return ValidationResult.Success;
+        }
+        if (((string)value).ToLower() == "human") {
+            return new ValidationResult("Type cannot be human");
+        }
+        return ValidationResult.Success;
+    }
+}
